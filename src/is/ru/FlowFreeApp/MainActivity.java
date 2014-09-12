1881a3719b0d531d2be445f8c7bc3a1324309309
Steinar <steinarv12@ru.is>
2014-09-12 14:02:31 +0000

    ButtonClick deleted

--- src/is/ru/FlowFreeApp/MainActivity.java
+++ src/is/ru/FlowFreeApp/MainActivity.java
@@ -17,14 +17,5 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main);
     }
 
-    public void buttonClick( View view ) {
-        Button button = (Button) view;
-        int id = button.getId();
-        if ( id == R.id.button_play ) {
-            startActivity( new Intent( this, PlayActivity.class ) );
-        }
-        else if ( id == R.id.button_color ) {
-            startActivity( new Intent( this, ColorListActivity.class ) );
-        }
-    }
+
 }
