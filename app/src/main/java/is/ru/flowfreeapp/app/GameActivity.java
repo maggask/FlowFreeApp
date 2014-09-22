82bb481655f3a4878f37832aca40f3dc7b8dc679
snussurofa <annalaufey92@gmail.com>
2014-09-22 21:14:13 +0000

    Merge branch 'master' of https://github.com/maggask/FlowFreeApp
    Conflicts:
    	app/src/main/java/is/ru/flowfreeapp/app/MainActivity.java

--- app/src/main/java/is/ru/flowfreeapp/app/GameActivity.java
+++ app/src/main/java/is/ru/flowfreeapp/app/GameActivity.java
@@ -2,6 +2,7 @@ package is.ru.flowfreeapp.app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -21,8 +22,19 @@ public class GameActivity extends Activity {
     }
 
     public void backClick(View view) {
-        Sound s = new Sound();
-        s.playSound(this);
+        SharedPreferences settings = getSharedPreferences( "SwitchPref", MODE_PRIVATE );
+        boolean soundOn = settings.getBoolean("soundSettings", false);
+        boolean vibrateOn = settings.getBoolean("vibrationSettings", false);
+
+        if(soundOn){
+            Sound s = new Sound();
+            s.playSound(this);
+        }
+        if(vibrateOn){
+            Vibration v = new Vibration();
+            v.vibrate(this);
+        }
+
         ImageView backButton = (ImageView)view;
         int backId = backButton.getId();
 
