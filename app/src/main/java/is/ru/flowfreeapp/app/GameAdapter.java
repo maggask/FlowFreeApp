58317f9b0a6ba520f77da5c226e0115a708f3d9f
snussurofa <annalaufey92@gmail.com>
2014-09-22 22:58:34 +0000

    Merge branch 'master' of https://github.com/maggask/FlowFreeApp
    Conflicts:
    	app/src/main/java/is/ru/flowfreeapp/app/MainActivity.java

--- app/src/main/java/is/ru/flowfreeapp/app/GameAdapter.java
+++ app/src/main/java/is/ru/flowfreeapp/app/GameAdapter.java
@@ -14,7 +14,7 @@ import android.database.sqlite.SQLiteDatabase;
 public class GameAdapter {
 
     SQLiteDatabase db;
-    dbHelper dbHelper;
+    DbHelper DbHelper;
     Context context;
 
     public GameAdapter(Context c) {
@@ -22,14 +22,14 @@ public class GameAdapter {
     }
 
     public GameAdapter openToRead() {
-        dbHelper = new dbHelper(context);
-        db = dbHelper.getReadableDatabase();
+        DbHelper = new DbHelper(context);
+        db = DbHelper.getReadableDatabase();
         return this;
     }
 
     public GameAdapter openToWrite() {
-        dbHelper = new dbHelper(context);
-        db = dbHelper.getWritableDatabase();
+        DbHelper = new DbHelper(context);
+        db = DbHelper.getWritableDatabase();
         return this;
     }
 
@@ -38,42 +38,42 @@ public class GameAdapter {
     }
 
     public long insertGame(int gid, boolean isComplete, int bestMove) {
-        String[] cols = dbHelper.TableGamesCols;
+        String[] cols = DbHelper.TableGamesCols;
         ContentValues contentValues = new ContentValues();
         contentValues.put(cols[1], ((Integer)gid).toString());
         contentValues.put(cols[2], isComplete ? "1" : "0");
         contentValues.put(cols[3], ((Integer)bestMove).toString());
 
         openToWrite();
-        long value = db.insert(dbHelper.TableGames, null, contentValues);
+        long value = db.insert(DbHelper.TableGames, null, contentValues);
         close();
         return value;
     }
 
     public long updateWonGame(int gid, boolean finished, int bestMove) {
-        String[] cols = dbHelper.TableGamesCols;
+        String[] cols = DbHelper.TableGamesCols;
         ContentValues contentValues = new ContentValues();
         contentValues.put(cols[1], ((Integer)gid).toString());
         contentValues.put(cols[2], finished ? "1" : "0");
         contentValues.put(cols[3], ((Integer)bestMove).toString());
 
         openToWrite();
-        long value = db.update(dbHelper.TableGames, contentValues, cols[1] + "=" + gid, null ); //package id, puzzle id, game id
+        long value = db.update(DbHelper.TableGames, contentValues, cols[1] + "=" + gid, null ); //package id, puzzle id, game id
         close();
         return value;
     }
 
     public Cursor queryGames() {
         openToRead();
-        Cursor cursor = db.query( dbHelper.TableGames,
-                dbHelper.TableGamesCols, null, null, null, null, null);
+        Cursor cursor = db.query( DbHelper.TableGames,
+                DbHelper.TableGamesCols, null, null, null, null, null);
         return cursor;
     }
 
     public Cursor queryGames(int sid) {
         openToRead();
-        String[] cols = dbHelper.TableGamesCols;
-        Cursor cursor = db.query( dbHelper.TableGames,
+        String[] cols = DbHelper.TableGamesCols;
+        Cursor cursor = db.query( DbHelper.TableGames,
                 cols, cols[1] + "=" + sid, null, null, null, null);
         return cursor;
     }
